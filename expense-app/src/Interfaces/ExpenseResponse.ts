0156f87b2c0fbe9b71f01cdf6aa571115061dcbd
vnagasravani <vnagasravani1998@gmail.com>
2020-03-13 11:37:15 +0530

    h

--- expense-app/src/Interfaces/ExpenseResponse.ts
+++
@@ -1,7 +0,0 @@
-import {ExpenseInfo} from './ExpenseInfo'
-export interface ExpenseResponse {
-    error: Boolean,
-    status: Number,
-    message: String,
-    data: ExpenseInfo   
-}
--- expense-app/src/Interfaces/Expenses.ts
+++
@@ -1,7 +0,0 @@
-import {ExpenseInfo} from './ExpenseInfo'
-export interface Expenses {
-    error: Boolean,
-    status: Number,
-    message: String,
-    data: [ExpenseInfo]   
-}
